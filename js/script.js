1c8133d6b9dcba24780cb06f86b4aac0dc37748a
Programozas Karrier <info@programozaskarrier.hu>
2021-04-20 11:38:29 +0200

    jegyes még jobb

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
-var db =prompt ( 'Add meg a darabszámot!')
+var db = prompt('Add meg a darabszámot!')
 var tip = prompt('Milyen típus?')
 
 
-var tipus = { student : 300,
-            adult :350,
-             retard : 280}[tip];
+var tipus = {
+    student: 300,
+    adult: 350,
+    retard: 280};
+/* ha a tipus után írd be a [tip]-et úgy is működik*/
+var tipusar = tipus[tip];
 
-var eng = db > 10 ? 0.9 : 1 ;
+var eng = db > 10 ? 0.9 : 1;
 
-var ar = db * tipus * eng ;
-
-
+var ar = db * tipusar * eng;
 
 
-alert (ar);
+
+
+alert(ar);
