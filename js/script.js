294bfbd864b598a62a5bdf0cd0846f12ec7803e6
Programozas Karrier <info@programozaskarrier.hu>
2021-04-20 11:13:08 +0200

    jegyes jobb második

--- js/script.js
+++ js/script.js
@@ -2,7 +2,9 @@ var db =prompt ( 'Add meg a darabszámot!')
 var tip = prompt('Milyen típus?')
 
 
-var tipus = tip ==='student' ? 300 : 350;
+var tipus = { student : 300,
+            adult :350,
+             retard : 280}[tip];
 
 var eng = db > 10 ? 0.9 : 1 ;
 
