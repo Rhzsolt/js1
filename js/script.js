4ee2d3a2fa524ec3b432d5a6329dff16dc794a30
rhzsolt@gmail.com <you@example.com>
2021-04-21 15:01:58 +0200

    0

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-var age = prompt('Add meg az életkorod4');
+var age = prompt('Add meg az életkorod!');
 
 switch (true) {
    case age <= 20:
       { alert('Fiatal') }
       break;
 
-   case age >20 && age < 30:
+   case age >20 && age < 60:
       { alert('Középkorú') }
       break;
    default: { alert('Öreg') }
