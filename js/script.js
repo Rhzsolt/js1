e24ee52a150e9e5353c98e8ac7523a681fd12f0e
Programozas Karrier <info@programozaskarrier.hu>
2021-04-23 19:31:32 +0200

    függvény elnevezés , feladat ,meghívás

--- js/script.js
+++ js/script.js
@@ -1,22 +1,18 @@
-var hetNapjai = new Date().getDay();
 
-switch (hetNapjai) {
 
-   case 0: alert('Vasárnap');
-      break;
+function füszerek()
 
-   case 1: alert('Hétfő');
-      break;
+ {
+    alert('só ')
+   alert('bors')}
 
-   case 2: alert('Kedd');
-      break;
 
-   case 3:  alert('Szerda') 
-      break;
+function rantottaSütés() 
+{ 
+   alert('tojás kell')
+   
+   alert('olajat rá')
+   alert('meg a többi');
+};
 
-   case 4: alert('Csütörtök') ;
-      break;
-
-   default: { alert('Nem túl jó !!') }
-}
-
+ rantottaSütés() ;füszerek();
